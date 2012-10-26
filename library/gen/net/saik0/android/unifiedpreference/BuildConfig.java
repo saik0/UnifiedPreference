a2d19bb65cdb350f22feaeec6a4bf15f9441c0e1
Joel Pedraza <github@saik0.net>
2012-10-26 09:42:44 -0400

    really untrack ignored files

--- library/gen/net/saik0/android/unifiedpreference/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.saik0.android.unifiedpreference;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
